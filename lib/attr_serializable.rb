3749ce0b34b35eb73b1debef817e5babbcd3272a
Joey Schoblaska <joey@zenwebapps.com>
2008-10-28 01:02:17 -0500

    workaround for problem in XmlSerializer when calling to_xml with :only and :include

--- lib/attr_serializable.rb
+++ lib/attr_serializable.rb
@@ -3,7 +3,30 @@ module AttrSerializable
   def self.included(base) #:nodoc:
     base.extend ClassMethods
   end
+  
+  # For some reason, using :include and :only in to_xml takes the attributes from the :only options
+  # and passes them along to each nested model. This method reogranizes the :include option to properly
+  # call each nested model's to_xml with its respective attribute whitelist.
+  def self.fix_includes(options = {})
+    begin
+      if options.keys.include?(:include) and options[:include].class.to_s == "Array"
+        models = []
+        options_include = {}
 
+        options[:include].each do |key|
+          klass = Kernel.const_get(key.to_s.classify)
+          options_include[key] = {:only => klass.serializable_attributes}
+        end
+        
+        return options_include
+      else
+        return options[:include]
+      end
+    rescue
+      return options[:include]      
+    end
+  end
+  
   module ClassMethods
 
     def attr_serializable(*attributes)
@@ -21,6 +44,7 @@ module AttrSerializable
     
     def to_xml(options = {}) 
       options[:only] ||= self.class.serializable_attributes
+      options[:include] = AttrSerializable.fix_includes(options)
       super(options)
     end
     
